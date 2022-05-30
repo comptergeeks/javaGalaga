8cbc70a046ccde532234419f0b5a26654ed8c0bb
Panav Kotha <panavkotha21@gmail.com>
2022-05-30 15:31:42 -0700

    creating the details for JFrame

--- src/com/company/myFrame.java
+++ src/com/company/myFrame.java
@@ -1,6 +1,15 @@
 package com.company;
-import javax.swing.JFrame;
 
-public class myFrame extends JFrame {
+import java.awt.Color;
+import java.awt.event.*;
+import javax.swing.*;
 
+
+public class MyFrame extends JFrame implements KeyListener { // implement the KeyListener interface in Java
+    
+    MyFrame() { // defining the frame panel
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // how the frame should close: closes when program terminates
+        this.setSize(500, 500); // panel size
+        this.setLayout(null); // want to set the layout to null because Galaga requires moving ojetc
+    }
 }
